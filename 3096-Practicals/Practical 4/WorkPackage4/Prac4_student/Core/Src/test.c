f4523e5d21278fb2da60d4bb272a35c012236024
Dante Webber <wbbdan003@myuct.ac.za>
2023-10-17 16:11:30 +0200

    First commit at lab

---
+++ 3096-Practicals/Practical 4/WorkPackage4/Prac4_student/Core/Src/test.c
@@ -0,0 +1,42 @@
+// #include <conio.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    printf("Hi");
+
+    // FILE* sine = fopen("/home/dantewebber/Documents/Dropbox/UCT/3rd_Year/2nd_Semester/EEE3096S/3096S_PLLTHI032_WBBDAN003/3096-Practicals/Practical 4/sine.csv", "r");
+    // FILE* tri = fopen("/home/dantewebber/Documents/Dropbox/UCT/3rd_Year/2nd_Semester/EEE3096S/3096S_PLLTHI032_WBBDAN003/3096-Practicals/Practical 4/triangle.csv", "r");
+    // FILE* saw = fopen("/home/dantewebber/Documents/Dropbox/UCT/3rd_Year/2nd_Semester/EEE3096S/3096S_PLLTHI032_WBBDAN003/3096-Practicals/Practical 4/sawtooth.csv", "r");
+
+    // if (!sine)
+    // printf("Can't open sine.csv\n");
+    // else if (!tri)
+    // printf("Can't open triangle.csv\n");
+    // else if (!saw)
+    // printf("Can't open sawtooth.csv\n");
+    // else {
+    //     char buffer[1024];
+
+    //     int row = 0;
+    //     int col = 0;
+
+    //     while(fgets(buffer, 1024, sine)) {
+    //         col = 0;
+    //         row++;
+
+    //         if (row == 1) continue;
+
+    //         char* value = strtok(buffer, ", ");
+
+    //         printf("%s", value);
+    //         value = strtok(NULL, ", ");
+    //     }
+    // }
+    // fclose(sine);
+    // fclose(tri);
+    // fclose(saw);
+
+    return 0;
+}
